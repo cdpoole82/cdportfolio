850f494046ed0538902b5f5406421845287e6440
cdpoole <poole.christopher@gmail.com>
2021-07-11 20:56:58 -0500

    Sunday night upload

--- src/components/NavBarMain.js
+++ src/components/NavBarMain.js
@@ -1,13 +1,18 @@
-import { FaGithub } from 'react-icons/fa'
-import { FaTh } from 'react-icons/fa'
+import { FaGithub, FaRegComment, FaTh } from 'react-icons/fa'
 
 function NavBarMain() {
+    
+    function showHint () {
+        window.alert("Left click, drag! Right click, drag! Zoom In! Zoom Out!");
+    }
+        
     return (
-        <div className='topnav fixed'>
-            <a href="#home" className="">HOME</a>
-            <a href="#portfolio" className=""><FaTh/> PROJECTS</a>
-            <a href="https://github.com/cdpoole82" rel="noreferrer" target="_blank" className=""><FaGithub/> GITHUB</a>
-            
+        <div className='topnav'>
+            <a href="#home">HOME</a>
+            <a href="#projects"><FaTh className='fa-icon'/>PROJECTS</a>
+            <a href="https://github.com/cdpoole82" rel="noreferrer" target="_blank"><FaGithub className='fa-icon'/>GITHUB</a>
+            <a href ="#hint" onClick={showHint}>Screen Hint<FaRegComment className='fa-icon-sub'/></a>
+                        
         </div>
     )
 }
