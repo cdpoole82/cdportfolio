3b6c27411b5b9948689572cb1c8d8046d8226a47
cdpoole <poole.christopher@gmail.com>
2021-07-13 12:28:22 -0500

    updated nav bar

--- src/components/NavBarMain.js
+++ src/components/NavBarMain.js
@@ -1,19 +1,22 @@
 import { FaGithub, FaRegComment, FaTh } from 'react-icons/fa'
 
-const NavBarMain = ({updateCSSDisplay}) => {
-
-    function showHint() {
+/*
+function showHint() {
         window.alert("Hold left or right mouse click and drag");
     }
+<a href="#hint" onClick={showHint}>Screen Hint<FaRegComment className='fa-icon-sub' /></a>
+*/
 
-                
+// 
+const NavBarMain = ({updateCSSDisplay}) => {
+                    
     return (
         <div>
             <div className='topnav'>
                 <a href="#home" onClick={() => updateCSSDisplay("Home")}>HOME</a>
                 <a href="#projects" onClick={() => updateCSSDisplay("Projects")}><FaTh className='fa-icon' />PROJECTS</a>
                 <a href="https://github.com/cdpoole82" rel="noreferrer" target="_blank"><FaGithub className='fa-icon' />GITHUB</a>
-                <a href="#hint" onClick={showHint}>Screen Hint<FaRegComment className='fa-icon-sub' /></a>
+               
             </div>
             
         </div>
