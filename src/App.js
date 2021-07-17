e507aa57324819fb152638f627a719eaf8ebce25
cdpoole <poole.christopher@gmail.com>
2021-07-17 16:40:18 -0500

    Added project components and menu to top nav. Updated CSS for more components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Introduction from './components/Introduction'
 import Stage from './components/Stage'
+import WFD from './components/WFD'
+import DND from './components/DND'
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import { FaTh } from 'react-icons/fa'
+import { MdRotate90DegreesCcw } from 'react-icons/md'
+import { FaDatabase } from 'react-icons/fa'
+import { GiSpikedDragonHead } from 'react-icons/gi'
 
 
 
@@ -17,7 +21,9 @@ function App() {
         <div >
           <nav className="topnav">
             <Link to="/Introduction">HOME</Link>
-            <Link to="/Stage"><FaTh className='fa-icon' />PROJECTS</Link>
+            <Link to="/Stage"><MdRotate90DegreesCcw className='fa-icon' />3D Animation</Link>
+            <Link to="/WFD"><FaDatabase className='fa-icon' />WFD Application</Link>
+            <Link to="/DND"><GiSpikedDragonHead className='fa-icon' />D&D Utility</Link>
           </nav>
           {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
@@ -30,7 +36,13 @@ function App() {
             </Route>
             <Route path="/Stage">
               <Stage/>
-            </Route>            
+            </Route>    
+            <Route path="/WFD">
+              <WFD />
+            </Route>     
+            <Route path="/DND">
+              <DND />
+            </Route>    
           </Switch>
           <Footer />
         </div>
---
+++ src/components/DND.js
@@ -0,0 +1,48 @@
+import herologo from "../images/heros.jpg"
+
+
+const Introduction = () => {
+       
+    return (
+        <div className="dnd">
+                                 
+            <main id="intromain">
+                               
+                <header className="introheader">
+                   <img src={herologo} alt="Workforce Director logo" style={{float:"left", marginRight:"30px", width:"200px",height:"200px"}} />
+                   <h1>Digital Dungeon Master</h1>
+                   <p>A C#.NET Game Utility Application</p>
+                </header>
+
+                
+                <section id="introsection">
+                    <h2>Project</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+
+                </section>
+
+                
+                <blockquote className="blockquoteintro">
+                    <p>
+                        blockquote text here for possilbe use...
+                    </p>
+                </blockquote>
+
+                
+            </main>
+            
+        </div>
+    )
+}
+
+export default Introduction
